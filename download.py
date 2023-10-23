4601ee437c7f2b60c2aa9eea8a6c307a499eae5c
houzeyu2683 <houzeyu2683@gmail.com>
2023-10-23 08:30:47 +0800

    a

--- download.py
+++ download.py
@@ -1,13 +1,13 @@
 import core
 
 channel = 'youtube'
-link = 'https://www.youtube.com/playlist?list=PLbyorRThEk_J4xy8T95K_dtbJBtn0aG_8'
-folder = './test/'
+link = 'https://www.youtube.com/playlist?list=PLbyorRThEk_LczvBPiBDNaejrjGQILDTO'
+folder = './【年代新聞】2023年04-06月/'
 
 reptile = core.Reptile(channel=channel, folder=folder)
 inventory = reptile.getInventory(link=link)
 
-thread = 1
+thread = 8
 pipe = core.Pipe(
     inventory=inventory, 
     channel=channel, 
