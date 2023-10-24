063f3fa52fc3732aa05f20c7b1c298ea37aca92b
houzeyu2683 <houzeyu2683@gmail.com>
2023-10-24 23:57:19 +0800

    update

--- split.py
+++ split.py
@@ -1,8 +1,8 @@
 import core
 
-folder = './【主播焦點新聞】東森新聞/'
-head = 0.01
-tail = 0.8
+folder = './chunk/3/'
+head = 0.00
+tail = 1
 
 detection = core.Detection(folder=folder, head=head, tail=tail)
-detection.makeFragment(thread=4)
+detection.makeFragment(thread=1)
