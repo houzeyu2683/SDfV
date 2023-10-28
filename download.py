c3d4020fba1bcef007f06d2fbdcdcd2c8352fb24
houzeyu2683 <houzeyu2683@gmail.com>
2023-10-28 14:19:19 +0800

    a

--- download.py
+++ download.py
@@ -1,12 +1,14 @@
 import core
 
 if(__name__=='__main__'):
+    #
     definition = core.argparse.ArgumentParser()
     definition.add_argument("--channel", default='youtube', type=str)
     definition.add_argument("--link", default='', type=str)
     definition.add_argument("--folder", default='', type=str)
     definition.add_argument("--thread", default=4, type=int)
     argument = definition.parse_args()
+    #
     channel = argument.channel
     link = argument.link
     folder = argument.folder
@@ -17,6 +19,7 @@ if(__name__=='__main__'):
         folder=folder,
         inventory=inventory
     )
+    #
     thread = argument.thread
     pipe.makeVideo(thread=thread)
     pass
