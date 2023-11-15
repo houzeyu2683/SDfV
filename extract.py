47ea5134c102b12bbf2df11cbc54de7f8e251d31
houzeyu2683 <houzeyu2683@gmail.com>
2023-11-15 16:12:21 +0800

    fix

--- extract.py
+++ extract.py
@@ -3,20 +3,18 @@ import core
 if(__name__=='__main__'):
     definition = core.argparse.ArgumentParser()
     definition.add_argument("--folder", default='', type=str)
-    definition.add_argument("--scope", default=[0.01, 0.9], type=list)
-    # definition.add_argument("--tail", default=0.9, type=float)
+    definition.add_argument("--scope", default=[0.01, 0.9], nargs='+', type=float)
     definition.add_argument("--thread", default=4, type=int)
     argument = definition.parse_args()
     #
     folder = argument.folder
-    head = argument.head
-    tail = argument.tail
+    scope = argument.scope
+
     thread = argument.thread
     #
     detection = core.Detection(
         folder=folder,
-        head=head, 
-        tail=tail
+        scope=scope
     )
     detection.makeFragment(thread=thread)
     pass
